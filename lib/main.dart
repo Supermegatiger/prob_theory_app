88cea30110d2f3ff896c1d0f98a5223febb3c596
melugsam <melugsam@mail.ru>
2023-05-13 17:18:45 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.light(
+            primary: Colors.grey.shade800,
+            background: Colors.white,
+            shadow: Colors.transparent,
+            outline: Colors.transparent),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.dark(
+          primary: Colors.white,
+          background: Colors.grey.shade900,
+          surface: Colors.transparent,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Generator'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +45,146 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  var timeDilation=false;
+  var globalList = [];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Center(child: Text(widget.title)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Определение вероятности'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('Задача 1'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                      CheckboxListTile(
+                        title:
+                            const Text('Задача 2'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Теоремы сложения и умножения вероятностей'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('CheckboxListTile with red background'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Полная вероятность. Формула Байеса'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('CheckboxListTile with red background'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Законы распределения вероятностей дискретных случайных величин'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('CheckboxListTile with red background'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Законы распределения вероятностей непрерывных случайных величин'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('CheckboxListTile with red background'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text('Числовые характеристики случайных величин'),
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title:
+                            const Text('CheckboxListTile with red background'),
+                        value: timeDilation,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            timeDilation = !timeDilation;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
